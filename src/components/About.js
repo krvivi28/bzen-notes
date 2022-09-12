390c5c3532b25e278116d3b114b200e9709c0046
vivek <krvivi28@gmail.com>
2022-09-12 14:17:09 +0530

    ver1.0

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => { 
+    return (
+        <div>
+            Be Zen, a part of ThrivingZen (OPC) Private Limited is an organization focused on making sustainable living hassle-free and affordable. Be Zen provides sustainable product recommendations across fashion, food, cosmetics, and household products. Our team members have experience working at Google and top-tier business schools such as INSEAD.
+        </div>
+    )
+}
+
+export default About
